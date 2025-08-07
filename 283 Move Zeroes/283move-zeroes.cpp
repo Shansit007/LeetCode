8ee90ca85b2a866ec3166884d3fefe4ac2dbc069
Shansit007 <9062731930046shansit@gmail.com>
2025-08-07 08:12:46 +0000

    Code challenge submission for question: 283 Move Zeroes

---
+++ 283 Move Zeroes/283move-zeroes.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int i = -1;
+        for(int j=0; j<nums.size(); j++){
+            if(nums[j]==0 && i== -1){
+                i=j;
+            }
+            else if(nums[j]!=0 && i!= -1){
+                swap(nums[j],nums[i]);
+                i++;
+            }
+        }
+    }
+};
